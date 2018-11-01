a8d2368e5bccfaa2e7b8f69aaee15ea6fd5a9fb0
Ahuvi <ahuvi.yearim@comm-it.com>
2018-11-01 12:09:53 +0200

    update test suite

--- test.js
+++ test.js
@@ -2,8 +2,24 @@ const Store = require('./index').Store;
 
 const instance = Store({ name: 'Jeff Wright' });
 
-instance.stream.subscribe(val => {
-    console.log('got instance value:', val);
-});
+try {
+    if (instance.value && instance.value.name === 'Jeff Wright') {
+        console.log('PASSED: Store initialization');
+    } else {
+        throw 'FAILED';
+    }
+} catch (err) {
+    console.warn('FAILED: Store initialization');
+}
 
-instance.publish(val => val.name = 'Dean Harvey');
+instance.publish(val => Object.assign({}, val, { name: 'Dean Harvey' }));
+
+try {
+    if (instance.value && instance.value.name === 'Dean Harvey') {
+        console.log('PASSED: Store publish');
+    } else {
+        throw 'FAILED';
+    }
+} catch (err) {
+    console.warn('FAILED: Store publish');
+}
